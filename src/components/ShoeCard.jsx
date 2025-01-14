f71bc8e94ac806a0cdf394807051483799c345e7
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-01-15 00:23:42 +0530

    border for shoeCard updated

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -9,8 +9,8 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
     <div
       className={`border-2 rounded-xl ${
         bigShoeImg === imgURL.bigShoe
-          ? 'border-white'
-          : 'border-black'
+          ? 'border-coral-red'
+          : 'border-transparent'
       } cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
