ed1266289ac3356ec489712a93849a671f82535e
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-01-18 13:31:30 +0530

    The products and quality section updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Services from './sections/Services'
 import SpecialOffer from './sections/SpecialOffer'
 import Subscribe from './sections/Subscribe'
 import Nav from './components/Nav'
+import SuperQuality from './sections/SuperQuality'
 
 const App=()=>{
 
@@ -14,14 +15,14 @@ const App=()=>{
     
     <main className = "relative">
       <Nav />
-      <section className = "w-full">
+      <section className = "xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
       <section className = "padding">
         <PopularProducts />
       </section>
       <section className = "padding">
-        <PopularProducts />
+        <SuperQuality />
       </section>
       <section className = "padding-x py-10">
         <Services />
@@ -31,7 +32,6 @@ const App=()=>{
       </section>
       <section className = "bg-pale-blue padding">
         <CustomerReviews />
-
       </section>
       <section className = "padding-x sm:py-32 py-16 w-full">
         <Subscribe />
